8750ad712f5cfe38c278924b46f790b5964b110e
DHARMESH SHARMA <sharmadharmesh1998@gmail.com>
2026-03-21 18:02:25 +0530

    small ui

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function LandingPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-8">Welcome to BudgetBee</h1>
+      <h1 className="text-2xl font-bold mb-8">Welcome to BudgetBee</h1>
       <Button text="Login" url="/login"/>
     </div>
     </div>
