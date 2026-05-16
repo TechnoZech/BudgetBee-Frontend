9fe6efa8bcedbc551c99237f704e7815a235ddd3
TechnoZech <sharmadharmesh1998@gmail.com>
2026-05-16 18:10:07 +0530

    Refactor layout in Transactions and Home components by adjusting padding for improved visual consistency

--- app/components/Transactions.tsx
+++ app/components/Transactions.tsx
@@ -34,7 +34,7 @@ const Transactions = () => {
 
 
 	return (
-		<div className="max-h-screen px-5">
+		<div className="max-h-screen px-2">
 			<div className="flex gap-5 pb-5">
 				<div className="flex-1 flex flex-col items-center justify-center h-20 rounded-xl border border-red-400 bg-red-100 text-red-700 shadow-sm dark:bg-red-900/30 dark:text-red-400 dark:border-red-800">
 					<span className="text-sm font-medium">Expense</span>
